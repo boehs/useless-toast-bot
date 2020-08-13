8b9d022663af8cec8ab010b259c85b46ec7f10fe
Scaledish <ebrobot2000@gmail.com>
2020-08-13 11:55:04 -0400

    made a sick embed :)

--- index.js
+++ index.js
@@ -3,6 +3,36 @@ const request = require('request');
 const fetch = require('node-fetch');
 require('dotenv-flow').config();
 const client = new Discord.Client();
+const aboutembed = {
+  title: "About",
+  description: "Ping For Toast. You **Ping**... I **DM** toast. I am possibly the most useless discord bot... but who the fuck ever wanted a useful discord bot? psssh. Amateurs. ",
+  color: 0xc49016,
+  footer: {
+    icon_url: 'https://cdn.discordapp.com/app-assets/743109027831218176/743114454115418222.png',
+    text: "This message was approved and paid for by the Ping For Toast administration lol"
+  },
+  thumbnail: {
+    url: 'https://cdn.discordapp.com/app-icons/743109027831218176/3806aab678834c0e8bd3daf037b7364a.png'
+  },
+  author: {
+    name: "By Ping For Toast (the dev... NOT the bot lol)",
+    icon_url: 'https://cdn.discordapp.com/avatars/543828521844342790/b3943bb9252490b8179b829976de7893.png?size=128'
+  },
+  fields: [
+    {
+      name: "Invite me!",
+      value: "invite me using https://discord.com/oauth2/authorize?client_id=743109027831218176&scope=bot"
+    },
+    {
+      name: "complaints?",
+      value: "DM **Ping for toast#7910**... OR make a issue at the link below!"
+    },
+    {
+      name: "The bot is open source!",
+      value: "Contribute or make a issue at https://github.com/Scaledi/useless-toast-bot"
+    }
+  ]
+};
 
 const config = {
   token: process.env.TOKEN,
@@ -25,7 +55,7 @@ client.on('message', msg => {
 
  client.on('message', msg => {
   if(msg.content.toLowerCase() === '!!about' || msg.content.toLowerCase() === 'about the toast' || msg.content.toLowerCase() === 'toast about') {
-    msg.reply('Ping For Toast. You Ping... I DM toast. I am possibly the most useless discord bot... but who the fuck ever wanted a useful discord bot? pssst. Amateurs. **invite me using https://discord.com/oauth2/authorize?client_id=743109027831218176&scope=bot**. **complaints? DM Ping for toast#7910**. or **make a issue at https://github.com/Scaledi/useless-toast-bot**. your call');
+    msg.reply({ embed: aboutembed });
     }
  });
 
